d7500d585d44a68b9b1672619ad59238392a3df4
c-degni <christivanndeg@gmail.com>
2026-01-28 17:15:04 -0500

    sim

--- EV4_ADBMS.ino
+++ EV4_ADBMS.ino
@@ -141,7 +141,7 @@ void measure_temp(bool open_wire_check) {
         for (int reading = 0; reading < 3 && thermistor_idx < 10; reading++) { // GPIO reading within group (~3 per group)
             for (int b = 0; b < num_boards; b++) {
                 uint16_t adc_code = ((uint8_t)response[b][reading * 2 + 1] << 8) | response[b][reading * 2];
-                cell_temp[b][thermistor_idx] = (float)adc_code * 0.00015f; // LSB represents 150 uV
+                cell_temp[b][thermistor_idx] = (float)adc_code * 0.00015f + 1.5f; // LSB represents 150 uV + 1.5V
             }
             thermistor_idx++;
         }
