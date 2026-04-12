4efd890edd6cd1acad50675670f97ea9c1a540ac
corbom22 <corbomichael@outlook.com>
2026-04-12 17:43:34 -0400

    init

--- COMMANDS.h
+++ COMMANDS.h
@@ -7,40 +7,48 @@
 
 extern int wire_cut;        //indicates position of wire break . "0" indicates normal operation with no break in the isoSPI line
 
-#define WRCFGA   0b0000000000000001      //Write Configuration Register Group A
-#define WRCFGB   0b0000000000100100      //Write Configuration Register Group B
-#define RDCFGA   0b0000000000000010      //Read Configuration Register Group A
-#define RDCFGB   0b0000000000100110      //Read Configuration Register Group B
-#define RDCVA    0b0000000000000100      //Read Cell Voltage Register Group A
-#define RDCVB    0b0000000000000110      //Read Cell Voltage Register Group B
-#define RDCVC    0b0000000000001000      //Read Cell Voltage Register Group C
-#define RDCVD    0b0000000000001010      //Read Cell Voltage Register Group D
-#define RDCVE    0b0000000000001001      //Read Cell Voltage Register Group E
-#define RDCVF    0b0000000000001011      //Read Cell Voltage Register Group F
-#define RDAUXA   0b0000000000001100      //Read Auxiliary Register Group A
-#define RDAUXB   0b0000000000001110      //Read Auxiliary Register Group B
-#define RDAUXC   0b0000000000001101      //Read Auxiliary Register Group C
-#define RDAUXD   0b0000000000001111      //Read Auxiliary Register Group D
-#define RDSTATA  0b0000000000010000      //Read Status Register Group A
-#define RDSTATB  0b0000000000010010      //Read Status Register Group B 
-#define WRSCTRL  0b0000000000010100      //Write S Control Register Group 
-#define WRPWM    0b0000000000100000      //Write PWM Register Group 
-#define WRPSB    0b0000000000011100      //Write PWM/S Control Register Group B 
-#define RDSCTRL  0b0000000000010110      //Read S Control Register Group 
-#define RDPWM    0b0000000000100010      //Read PWM Register Group 
-#define RDPSB    0b0000000000011110      //Read PWM/S Control Register Group B 
-#define STSCTRL  0b0000000000011001      //Start S Control Pulsing and Poll Status
-#define CLRSCTRL 0b0000000000011000      //Clear S Control Register Group 
-#define CLRCELL  0b0000011100010001      //Clear Cell Voltage Register Groups 
-#define CLRAUX   0b0000011100010010      //Clear Auxiliary Register Groups
-#define CLRSTAT  0b0000011100010011      //Clear Status Register Groups 
-#define PLADC    0b0000011100010100      //Poll ADC Conversion Status 
-#define DIAGN    0b0000011100010101      //Diagnose MUX and Poll Status 
-#define WRCOMM   0b0000011100100001      //Write COMM Register Group g
-#define RDCOMM   0b0000011100100010      //Read COMM Register Group 
-#define STCOMM   0b0000011100100011      //Start I 2C/SPI Communication 
-#define MUTE     0b0000000000101000      //Mute Discharge
-#define UNMUTE   0b0000000000101001      //Unmute Discharge 
+#define WRCFGA   (0b0000000000000001)         // Write Configuration Register Group A
+#define WRCFGB   (0b0000000000100100)         // Write Configuration Register Group B
+#define RDCFGA   (0b0000000000000010)         // Read Configuration Register Group A
+#define RDCFGB   (0b0000000000100110)         // Read Configuration Register Group B
+#define RDCVA    (0b0000000000000100)         // Read Cell Voltage Register Group A
+#define RDCVB    (0b0000000000000110)         // Read Cell Voltage Register Group B
+#define RDCVC    (0b0000000000001000)         // Read Cell Voltage Register Group C
+#define RDCVD    (0b0000000000001010)         // Read Cell Voltage Register Group D
+#define RDCVE    (0b0000000000001001)         // Read Cell Voltage Register Group E
+#define RDCVF    (0b0000000000001011)         // Read Cell Voltage Register Group F
+#define RDSVA    (0b0000000000000011)         // Read S Voltage Register Group A
+#define RDSVB    (0b0000000000000101)         // Read S Voltage Register Group B
+#define RDSVC    (0b0000000000000111)         // Read S Voltage Register Group C
+#define RDSVD    (0b0000000000001101)         // Read S Voltage Register Group D
+#define RDSVE    (0b0000000000001110)         // Read S Voltage Register Group E
+#define RDSVF    (0b0000000000001111)         // Read S Voltage Register Group F
+#define RDAUXA   (0b0000000000011001)         // Read Auxiliary Register Group A
+#define RDAUXB   (0b0000000000011010)         // Read Auxiliary Register Group B
+#define RDAUXC   (0b0000000000011011)         // Read Auxiliary Register Group C
+#define RDAUXD   (0b0000000000011111)         // Read Auxiliary Register Group D
+#define RDSTATA  (0b0000000000110000)         // Read Status Register Group A
+#define RDSTATB  (0b0000000000110001)         // Read Status Register Group B  
+#define WRPWMA   (0b0000000000100000)         // Write PWM Register Group A
+#define WRPWMB   (0b0000000000100001)         // Write PWM Register Group B 
+#define RDPWMA   (0b0000000000100010)         // Read PWM Register Group A
+#define RDPWMB   (0b0000000000100011)         // Read PWM Register Group B  
+#define CLRCELL  (0b0000011100010001)         // Clear Cell Voltage Register Groups 
+#define CLRAUX   (0b0000011100010010)         // Clear Auxiliary Register Groups
+#define PLADC    (0b0000011100011000)         // Poll ADC Conversion Status 
+#define WRCOMM   (0b0000011100100001)         // Write COMM Register Group g
+#define RDCOMM   (0b0000011100100010)         // Read COMM Register Group 
+#define STCOMM   (0b0000011100100011)         // Start I 2C/SPI Communication 
+#define MUTE     (0b0000000000101000)         // Mute Discharge
+#define UNMUTE   (0b0000000000101001)         // Unmute Discharge
+
+#define ADCV     (0b0000001001100000)         // Start Cell Voltage ADC Conversion and Poll Status - C-ADC
+#define ADSV     (0b0000000101101000)         // Start Cell Voltage ADC Conversion and Poll Status - S-ADC
+#define ADAX     (0b0000010010010000)         // Start GPIO ADC Conversion and Poll Status
+#define OW_GPIO  (0b0000000110000000)         // GPIO Open Wire Detection Bit
+#define OW_ODD   (0b0000000000000010)         // Cell Open Wire Odd Bits
+#define OW_EVEN  (0b0000000000000001)         // Cell Open Wire Even Bits
+#define ITEMP    (0b0000000001000011)         // Die Temperature Bits
 
 //// configuration dependent commands ////
 
--- LTC6813.ino
+++ LTC6813.ino
@@ -68,6 +68,10 @@ float open_circuit_voltage[num_boards][num_cells];
 float pack_voltage = 0;          //sum of cell voltages
 float cell_temp[num_boards][9];  //most recent cell temperatures. Contans raw voltage data for the duration of open wire checks
 float die_temps[num_boards];     //most recent sense board LTC6813 die temps
+float min_cell_voltage = 0;
+float max_cell_voltage = 0;
+float min_cell_temp = 0;
+float max_cell_temp = 0;
 
 //sense board flags
 float GPIO_open_wire[num_boards][9];
@@ -738,39 +742,47 @@ void send_command(uint16_t command) {
 
 void read_register_group(uint16_t command, uint8_t response[num_boards][6]) {  //register group is always 6 bytes
 
-  uint16_t pec;
-  uint8_t pec0;
-  uint8_t pec1;
+  uint8_t ccmd; // command counter
+  uint16_t rx_pec10; // Recieved and parsed 10 bit data PEC
+  uint16_t calc_pec10; // Calculated 10 bit data PEC
   uint8_t response_pec0;
   uint8_t response_pec1;
 
   send_command(command);
 
   for (int i = 0; i < num_boards; i++) {
-    for (int j = 0; j < 6; j++) {
-      response[i][j] = SPI.transfer(0b11111111);  // Send dummy byte to receive data
-      //Serial.println(response[i][j], BIN);
-    }
-    response_pec0 = SPI.transfer(0xFF);
-    response_pec1 = SPI.transfer(0xFF);
-    pec = pec15_calc(6, response[i]);
-    pec1 = pec >> 0;
-    pec0 = pec >> 8;
+      for (int j = 0; j < 6; j++) {
+          response[i][j] = SPI.transfer(0b11111111); // Send dummy byte to receive data
+          // Serial.println(response[i][j], BIN);
+      }
 
-    if (response_pec0 != pec0 || response_pec1 != pec1) {  //this recursion needs fixed
-      Serial.println("pec error");
-      wakeup_sleep(num_boards + 1);
-      //read_register_group(command, response);
-    }
+      response_pec0 = SPI.transfer(0xFF); // response PEC = command counter + PEC, needs to parsed
+      response_pec1 = SPI.transfer(0xFF);
+      
+      // Extract command counter and received 10-bit PEC from the ADBMS6830B readback format:
+      // PEC0 = [CCNT5..0 | PEC9..8], PEC1 = [PEC7..0]
+      ccmd = (response_pec0 >> 2) & 0x3F;
+      rx_pec10 = ((uint16_t)(response_pec0 & 0x03) << 8) | response_pec1;
+
+      calc_pec10 = pec10_calc_data_ccnt(response[i], ccmd);
+
+      /*
+      if (rx_pec10 != (calc_pec10 & 0x3FF)) {
+          Serial.println("PEC Error - Data PEC Mismatch");
+          wakeup_sleep(NUM_BOARDS + 1);
+      }
+      */
+      
+      
   }
 
-  pec = pec15_calc(6, response[0]);  //this needs fixed to include multiple boards
-
-  // Serial.println("response pec");
-  // Serial.println(response_pec0, BIN);
-  // Serial.println(response_pec1, BIN);
-  // Serial.println("calculated pec");
-  // Serial.println(pec, BIN);
+  
+  // Debug code
+  Serial.print("response pec ");
+  Serial.println(rx_pec10, HEX);
+  Serial.print("calculated pec ");
+  Serial.println(calc_pec10, HEX);
+  
 
   digitalWrite(CS, HIGH);
 }
@@ -807,10 +819,17 @@ void poll_ADC(uint16_t command, bool curr_measure) {
   }
 
   int num_polls = 0;
-  while (return_data == 0) {                 //This needs a timeout condition
-    return_data = SPI.transfer(0b11111111);  // Send dummy byte to receive data
+  uint32_t adc_poll_start_time = millis();
+
+  while (return_data == 0) {
+    return_data = SPI.transfer(0b11111111); // Send dummy byte to receive data
     num_polls++;
-  }
+    if(millis() - adc_poll_start_time > 1000){          // 1000ms is a made up number and should be reviewed. This previously had no timeout.
+        Serial.println("ADBMS6830B ADC TImeout Error");
+        break;
+    }
+}
+
   // Serial.println("ADC Conversion Done!");
   // Serial.println(num_polls);
 
@@ -837,13 +856,14 @@ void measure_voltage() {  //18 millisecond execution time
       //Serial.print('j');
       //Serial.println(j);
       for (int k = 0; k < 3 && i * 3 + k < num_cells; k++) {                                                         //cell number within register group
-                                                                                                                     //Serial.print('k');
-                                                                                                                     //Serial.println(k);
-        cell_voltage[j][i * 3 + k] = (float)(((uint8_t)response[j][k * 2 + 1] << 8) | response[j][k * 2]) * 0.0001;  //LSB represents 100 uV
-        pack_voltage = pack_voltage + cell_voltage[j][i * 3 + k];
+        int16_t adc_code = (int16_t)(((uint16_t)response[j][k * 2 + 1] << 8) | response[j][k * 2]);
+        cell_voltage[j][i * 3 + k] = (float)adc_code * 0.00015f + 1.5f; // LSB represents 150 uV, +1.5v offset
+        pack_voltage += cell_voltage[j][i * 3 + k];
       }
     }
   }
+
+  // Update open circuit voltage if applicable
   if (current < 0.2 and current > -0.2) {
     for (int i = 0; i < num_boards; ++i) {
       for (int j = 0; j < num_cells; ++j) {
@@ -851,6 +871,11 @@ void measure_voltage() {  //18 millisecond execution time
       }
     }
   }
+
+  // Update min, max voltage
+  min_cell_voltage = cell_voltage[0][0];
+  max_cell_voltage = cell_voltage[0][0];
+  min_max<num_boards, num_cells>(cell_voltage, &min_cell_voltage, &max_cell_voltage);
 
   new_voltage = true;
 
@@ -1364,4 +1389,36 @@ void wakeup_idle(uint8_t total_ic) {        //idle after 4.3 ms of no isoSPI act
     digitalWrite(CS, HIGH);
   }
   delayMicroseconds(1);  //This delay is absolutely needed: t5 in datasheet - CSB Rising Edge to CSB Falling Edge >= 0.65us
+}
+
+uint16_t pec10_update_bit(uint16_t rem, uint8_t in_bit) {
+    // CRC10 width=10, poly without x^10 term: x^7 + x^3 + x^2 + x + 1 => 0x08F
+    const uint16_t poly = 0x008F;
+    const uint16_t mask = 0x03FF;
+
+    uint8_t fb = ((rem >> 9) & 1u) ^ (in_bit & 1u);  // feedback bit
+    rem = (uint16_t)((rem << 1) & mask);
+
+    if (fb) 
+        rem ^= poly;
+
+    return rem;
+}
+
+uint16_t pec10_calc_data_ccnt(const uint8_t *data6, uint8_t ccnt6) {
+    uint16_t rem = 0x0010; // initial value = 0000010000 :contentReference[oaicite:2]{index=2}
+
+    // 48 data bits, MSB-first
+    for (uint8_t i = 0; i < 6; i++) {
+        uint8_t d = data6[i];
+        for (int8_t b = 7; b >= 0; b--) {
+            rem = pec10_update_bit(rem, (d >> b) & 1u);
+        }
+    }
+
+    // 6 CCNT bits, MSB-first: CCNT[5]..CCNT[0] live in PEC0[7:2] :contentReference[oaicite:3]{index=3}
+    for (int8_t b = 5; b >= 0; b--)
+        rem = pec10_update_bit(rem, (ccnt6 >> b) & 1u);
+
+    return (rem & 0x03FF);
 }
