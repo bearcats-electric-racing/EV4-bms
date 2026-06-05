b000b3989fa5bd34b67f56f264c1a4dfa1a4346a
Patrick Mumford <mumforpd@mail.uc.edu>
2026-06-04 20:56:41 -0400

    Reset Sense Board number to 10

--- CONFIGURE.h
+++ CONFIGURE.h
@@ -17,7 +17,7 @@ const int watchdog_timeout = 0;  //watchdog timeout (in seconds). setting to 0 w
 const float open_wire_threshold = 0.30; // % voltage attenuation tolerance during open wire check - attenuation exceeding this value will flag an open wire
 
 //architecture
-const int num_boards = 1;
+const int num_boards = 10;
 const int num_cells = 14;       //cells per board
 const int num_parrallel = 1;
 
